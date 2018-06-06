1e65b1fcd995cfdf1abb2091a6e56842768c9ce6
MaZderMind <git@mazdermind.de>
2018-06-06 22:55:36 +0200

    preparations for selecting an preliminary b-source

--- voctogui/lib/videomixcontroller.py
+++ voctogui/lib/videomixcontroller.py
@@ -6,15 +6,24 @@ class VideoMixControllerCls(object):
         self.log = logging.getLogger('VideoMixController')
 
         self.composite_mode = None
-        self.video_a = None
+        self._video_a = None
         self.video_b = None
 
         self.clear_handler = []
+        self.preliminary_b_handler = []
+
+    @property
+    def video_a(self):
+        return self._video_a
+
+    @video_a.setter
+    def video_a(self, video_a):
+        self._video_a = video_a
+        self.select_preliminary_b()
 
     def apply(self):
         self.log.info("apply %s %s %s", self.composite_mode, self.video_a, self.video_b)
         self.clear()
-        pass
 
     def clear(self):
         self.log.info("clear")
@@ -22,12 +31,21 @@ class VideoMixControllerCls(object):
         self.composite_mode = None
         self.video_b = None
         self.video_b = None
-
-        for handler in self.clear_handler:
-            handler()
+        self.call_clear_handler()
 
     def register_clear_handler(self, cb):
         self.clear_handler.append(cb)
+
+    def call_clear_handler(self):
+        for handler in self.clear_handler:
+            handler()
+
+    def register_preliminary_b_handler(self, cb):
+        self.preliminary_b_handler.append(cb)
+
+    def call_preliminary_b_handler(self, preliminary_b):
+        for handler in self.preliminary_b_handler:
+            handler(preliminary_b)
 
     def set_source_for_channel(self, channel, source):
         if channel == 'a':
@@ -36,6 +54,9 @@ class VideoMixControllerCls(object):
             self.video_b = source
         else:
             raise RuntimeError('Unexpected Channel ' + channel)
+
+    def select_preliminary_b(self):
+        pass
 
 
 VideoMixController = VideoMixControllerCls()
